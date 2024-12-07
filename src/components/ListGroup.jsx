fd62b0fbafa8508b61dcb8f0d25417d569d73032
Vivek Bind <vivekb.ttl@tatamotors.com>
2024-12-07 06:58:57 +0530

    ok

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -9,16 +9,19 @@ function ListGroup() {
         'London',
         'Tokyo'
     ];
-    //items = [];
+    items = [];
 
     
-  //  const Message = items.length == 0 ? <p>No item found</p> : null
+
+    const getMessage = () => {
+        return items.length == 0 ? <p>No item found</p> : null;
+    }
 
     
     return (
         <>
             <h2>List</h2>
-          
+            { getMessage()}
         <ul className="list-group">
                 {items.map(item => (
                     <li key = {item}>{item}</li>))}
